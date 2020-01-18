474a5dbd0c9c986c632b31daf60a942f479ae113
anderson <anderdavid86@hotmail.com>
2020-01-17 21:25:28 -0500

    Se sigue otro ejemplo el cual implementa bootstrap...no se lo termina debido a que es otro chat grupal

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var server= require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+users =[];
+connections =[];
+
+server.listen(process.env.PORT || 3000);
+console.log('Server running...');
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index1.html');
 });
 
-io.on('connection', function(socket){
-    socket.on('chat message', function(msg){
+io.sockets.on('connection',function(socket){
+  connections.push(socket);
+  console.log("Connected: %s sockets connected",connections.length);
+  
+  //Disconect 
+  socket.on("disconnect",function(data){
+    connections.splice(connections.indexOf(socket),1);
+    console.log('Disconnect: %s socket connected',connections.length);
+  }) 
+
+  //Send msg
+  socket.on("send message",function(data){
+    console.log(data);
+    io.sockets.emit("new message",{msg:data});
+  });
+
+})
+
+/* io.on('connection', function(socket){
+    socket.on('chat message', function(msg){ 
       console.log(msg);
       io.emit('chat message', msg);
     });
 });
-
-
 
 io.on('connection', function(socket){
     console.log('a user connected');
@@ -22,7 +44,6 @@ io.on('connection', function(socket){
     });
 });
 
-
 http.listen(3000, function(){
   console.log('listening on *:3000');
-});
+}); */
